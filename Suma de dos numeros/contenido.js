b4c5413afefec14a63087a299cd5400ebc6c0fd9
AngelArgumedo <79815276+AngelArgumedo@users.noreply.github.com>
2021-03-07 21:13:40 -0500

    sum of two numbers
    simple program where I ask the user to write two values and check if they are numeric and then perform the sum.

---
+++ Suma de dos numeros/contenido.js
@@ -0,0 +1,21 @@
+/*
+	Angel David Argumedo Guerra
+
+	Suma de dos numeros.
+*/
+
+alert("Este programa suma dos numeros y muestra el resultado.")
+var num1= new Number(prompt("Digite un numero.")); //Aqui declaro la variable del primer numero y especifico que es de caracter numerico
+var num2= new Number(prompt("Digite otro numero.")); 
+var resultado; //Aqui declaro la variable del resultado no especifique el tipo porque no hace falta para este caso
+
+if (isNaN(num1)) { //Aqui digo si num1 no es numerico entonces lanza un alert diciendo que no es un numero
+	alert("El primer valor digitado no es numerico.");
+}else if (isNaN(num2)) { //Aqui digo si num2 no es numerico entonces lanza un alert diciendo que no es un numero
+	alert("El segundo valor digitado no es numerico.");
+}else {
+	// Aqui no mas es realizar la operacion de suma y mostrar el resultado
+	resultado= num1 + num2;
+	alert("El resultado de la suma es: "+resultado);
+}
+
