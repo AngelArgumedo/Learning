aa349ebe887ad17e2040b0e3c8d891631d7fd5fc
AngelArgumedo <79815276+AngelArgumedo@users.noreply.github.com>
2021-03-09 19:26:18 -0500

    Eventos
    Este programa es una practica de trabajar con eventos por primera vez.

---
+++ Eventos Pregunta con respuesta multiple/contenido.js
@@ -0,0 +1,30 @@
+//Trabajando con el Eventos
+
+var respuestaA = document.getElementById("a");
+var respuestaB = document.getElementById("b");
+var respuestaC = document.getElementById("c");
+var respuestaD = document.getElementById("d");
+var respuestaE = document.getElementById("e");
+
+
+a.onclick = function(){
+	respuestaA.innerHTML = "a) 2 = Es correcto";	
+}
+
+b.onclick = function(){
+	respuestaB.innerHTML = "b) 4 = Es falso";	
+}
+
+c.onclick = function(){
+	respuestaC.innerHTML = "c) 5 = Es falso";	
+}
+
+d.onclick = function(){
+	respuestaD.innerHTML = "d) 6 = Es falso";	
+}
+
+e.onclick = function(){
+	respuestaE.innerHTML = "e) 10 = Es falso";	
+}
+
+
